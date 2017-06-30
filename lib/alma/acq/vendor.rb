96d292927808ee89a7f4b75f098c8610d1271a3f
Chad Nelson <chad.nelson@temple.edu>
2017-06-30 08:47:37 -0400

    Make vendor actually work

--- lib/alma/acq/vendor.rb
+++ lib/alma/acq/vendor.rb
@@ -9,7 +9,7 @@ module Alma
       attr_accessor :code
 
       def post_initialize
-        @code = response['vendor']['code'].to_s
+        @code = @raw_record['vendor']['code'].to_s
       end
 
       def to_xml
@@ -23,49 +23,71 @@ module Alma
 
 
       def push_update!
-        local_params = { vendorCode: code, body: to_xml }
-        params = query_merge(local_params)
-        resources.almaws_v1_acq.vendors_vendorCode.put(params)
+        local_params = {
+          vendorCode: code,
+          body: to_xml,
+          headers: { "Content-Type" => 'application/xml'}
+        }
+        params = self.class.query_merge(local_params)
+        self.class.resources.almaws_v1_acq.vendors_vendorCode.put(params)
       end
 
       def vendor_libs_xpath
-        "/vendor/vendor_libraries"
+        "/vendor/vendor_libraries/library"
+      end
+
+      def vendor_account_library_xpath
+        "/vendor/accounts/account/account_libraries/library"
       end
 
 
-      def add_vendor_library(code, desc)
+      def add_vendor_library(libcode, desc)
         # Assumes the first library is Temple Alma organization
         # And that only a single Library would be in the secondary place
 
         fragment =
         "<library include_sub_units=\"false\">"\
-        "<code desc=\"#{desc}\">#{code}</code>"\
+        "<code desc=\"#{desc}\">#{libcode}</code>"\
         "</library>".strip
 
         node = Nokogiri::XML.fragment(fragment)
 
 
-        xml = Nokogiri::XML(to_xml)
-        vendor_libraries  = xml.xpath(vendor_libs_xpath)
+        xml = Nokogiri::XML(response.response.body)
+        vendor_libraries_size  = xml.xpath(vendor_libs_xpath).length
+        vendor_account_libs_size = xml.xpath(vendor_account_library_xpath).length
 
-        if vendor_libraries.length >= 1
-          vendor_libraries.pop
-          binding.pry
-        end
-
-        binding.pry
-
-        xml.xpath(vendor_libs_xpath) << node
-        binding.pry
-
-        update(xml.to_s)
-      end
-
-      def exclude_sub_units!
-        vendor_libraries.each do |library|
+        xml.xpath(vendor_libs_xpath).each do |library|
           library.attributes["include_sub_units"].value = "false"
         end
-        update(parsed_xml.to_s)
+
+        xml.xpath(vendor_account_library_xpath).each do |library|
+          library.attributes["include_sub_units"].value = "false"
+        end
+
+        unless xml.xpath(vendor_libs_xpath).last.text.eql? libcode
+          if vendor_libraries_size >= 2
+            puts "Deleting last account lib for #{code}"
+            xml.xpath(vendor_libs_xpath).last.remove
+          end
+          puts "Adding #{libcode} to last vendor lib for #{code}"
+          xml.xpath(vendor_libs_xpath).last.next = node.dup
+        else
+          puts "#{libcode} already the last vendor lib for #{code} "
+        end
+
+        unless xml.xpath(vendor_account_library_xpath).last.text.eql? libcode
+          if vendor_account_libs_size >= 2
+            puts "Deleting last account lib for #{code}"
+            xml.xpath(vendor_account_library_xpath).last.remove
+          end
+          puts "Adding #{libcode} to last account lib for #{code}"
+          xml.xpath(vendor_account_library_xpath).last.next = node
+        else
+          puts "#{libcode} already the last account lib for #{code} "
+        end
+
+        update(xml.to_s)
       end
 
       class << self
